68390903b9f335c5db6fc3efce535981ccdcb8f5
erin hubbard <erinhubbard1981@gmail.com>
2018-02-05 22:45:37 -0500

    all parts functioning properly

--- assets/Train.js
+++ assets/Train.js
@@ -38,8 +38,6 @@ $("#addTrain").on("click",function() {
    
   //pushes new train info to database
   database.ref().push(newTrain);
-  console.log("new train added");
-
   
   //clears input for next submission
   $("#thisTrain").val("");
@@ -49,26 +47,18 @@ $("#addTrain").on("click",function() {
 });
 
 database.ref().on("child_added", function(childSnapshot, prevChildKey) {
-	// console.log(childSnapshot.val());
-
-  var firstTrain = childSnapshot.val().arrival;
+	var firstTrain = childSnapshot.val().arrival;
   var trainName = childSnapshot.val().train;
   var destinationName = childSnapshot.val().destination; 
   var frequencyValue = childSnapshot.val().frequency;
-
   var currentTime = moment().format("hh.mm");
-  //var firstTrainConverted = moment(currentTime, "hh:mm").subtract(1, "years");
-  //console.log(firstTrainConverted);
-  var timeDiff = moment().diff(moment(firstTrain, "minutes"));
-  // console.log(moment(firstTrain, "minutes"));
-  // console.log(timeDiff);
+  var firstTrainConverted = moment(firstTrain, "hh:mm").subtract(1, "years");
+  var timeDiff = moment().diff(moment(firstTrainConverted, "minutes"));
   var remainder = timeDiff % frequencyValue;
-  console.log(remainder);
   var timeUntilValue = frequencyValue - remainder;
-  console.log('timeUntilValue: ',timeUntilValue);
-  var nextArrival = moment().add(timeUntilValue, "minutes").subtract(1, "years").format("hh:mm");
-  console.log('nextArrival:', nextArrival);
-
+  var nextArrival = moment().add(timeUntilValue, "minutes");
+  var nextArrival = moment(nextArrival).format("hh:mm A");
+  
   //updates the DOM with new train info back from database
 $("#table").append("<tr><td>" + trainName + "</td><td>" + destinationName + "</td><td>" +
   frequencyValue + "</td><td>" + nextArrival + "</td><td>" + timeUntilValue + "</td></tr>");
